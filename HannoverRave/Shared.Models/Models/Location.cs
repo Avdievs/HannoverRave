80317dfad77b0f17a5f5cc2f4101085c7ee680b6
Avdievs <avdievs@googlemail.com>
2019-02-13 19:54:56 +0100

    New project Shared.Models created.

---
+++ HannoverRave/Shared.Models/Models/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Models
+{
+    public class Location
+    {
+        public int LocationId { get; set; }
+        public string Address { get; set; }
+        //TODO: GeoCoordinate
+    }
+}
