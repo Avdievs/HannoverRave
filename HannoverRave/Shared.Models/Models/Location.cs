7c0622df41f79695852ffd93c911e91ca92425b6
Avdievs <avdievs@googlemail.com>
2019-02-13 19:52:48 +0100

    Cleaned

--- HannoverRave/Shared.Models/Models/Location.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared.Models
-{
-    public class Location
-    {
-        public int LocationId { get; set; }
-        public string Address { get; set; }
-        //TODO: GeoCoordinate
-    }
-}
