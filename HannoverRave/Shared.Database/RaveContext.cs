4b3f8a397e02d14d03c4ba5558d9a79e4f01e766
Avdievs <avdievs@googlemail.com>
2019-02-13 22:10:06 +0100

    Shared.Models deleted, because i need Core 2 SDK. Project Shared.Database created.

---
+++ HannoverRave/Shared.Database/RaveContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Database
+{
+    public class RaveContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+        public DbSet<DJ> DJs { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Location> Locations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(@"Seserver=localhost;database=HannoverRave;user=root;password=681567da");
+        }
+    }
+}
