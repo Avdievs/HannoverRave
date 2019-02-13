5f472f432caea723a821a151b9759798010a757d
Avdievs <avdievs@googlemail.com>
2019-02-13 19:59:09 +0100

    New Project Shared.Models created.

---
+++ HannoverRave/Shared.Models/Models/Enums/EventType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shared.Models
+{
+    [Flags]
+    public enum EventType
+    {
+        Club = 1,
+        OpenAir = 2,
+        Festival = 4
+    }
+}
---
+++ HannoverRave/Shared.Models/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shared.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Location Location { get; set; }
+        public EventType EventType { get; set; }
+        public IEnumerable<DJ> DJs { get; set; }
+        //TODO: Logo or a video
+    }
+}
