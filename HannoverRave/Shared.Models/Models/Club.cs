33eafc4a4e0a3409c687a7b9cdac9c845e18db1a
Avdievs <avdievs@googlemail.com>
2019-02-14 00:46:23 +0100

    Shared.Models Net.Standarts 2

---
+++ HannoverRave/Shared.Models/Models/Club.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Models
+{
+    public class Club
+    {
+        public int ClubId { get; set; }
+        public string Name { get; set; }
+        public Location Location { get; set; }
+    }
+}
---
+++ HannoverRave/Shared.Models/Models/Enums/EventType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shared.Models
+{
+    [Flags]
+    public enum EventType : int
+    {
+        Club = 1,
+        OpenAir = 2,
+        Festival = 4
+    }
+}
